c75a8d5adf4ea9ce176d6f2c80720fe7bc3c0a29
Dylan <dquesada186@gmail.com>
2017-11-29 09:23:13 -0600

    tweaked functions to return proper values, dropped in userInput function, assembled main function

--- reverseOrder.js
+++ reverseOrder.js
@@ -1,19 +1,15 @@
 "use strict";
 
-// function stringToArray(string){
-// 	var stringArray = [stringlength];
-// 	for (var i = 0; i > string.length; i++){
-// 		stringArray[i] = string.charAt(i);
-// 	}
-// 	return stringArray;
-// }
 
-// var x = "cat";
-// alert(stringToArray(x));
-// // function reverseArray(array){
-// // 	var newArray = [array.length];
-// // 	for (var i = 0; i < array.length; i++) {
-// // 	array.length
+
+function userInput(message){
+	if(message != null){
+		return prompt(message);
+	}
+	else{
+		return prompt("Enter user input:");
+	}
+}
 
 function reverseString(string){
 	var original = string;
@@ -21,7 +17,7 @@ function reverseString(string){
 	for (var i = 0; i < original.length; i++) {
 		reversed[i] = original.charAt(original.length - i - 1);
 	}
-	return reversed.toString();
+	return reversed;
 }
 
 
@@ -33,15 +29,14 @@ function concatonateArrayToCommalessString(array){
 	return outputString;
 
 }
-var array = ["x", "y", "z", 1];
-var x = array[1].concat(array[2]);
 
-// function removeCommasWithoutRemovingOriginalCommas(string){
-// 	var original = string;
-// 	for (var i = 1; i < original.length; i+=2) {
-// 		commaless i]
-// 	}
-// }
-console.log(reverseString("popcorn"));
-console.log(x);
-console.log(concatonateArrayToCommalessString(array));
+function runReverseOrder(string){
+	var finalProduct = userInput();
+	finalProduct = reverseString(finalProduct);
+	finalProduct = concatonateArrayToCommalessString(finalProduct);
+	alert(finalProduct);
+	return finalProduct;
+}
+
+
+console.log(runReverseOrder());
